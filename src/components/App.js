5eca5a543f46f4c2de5ea5baf4538aaab0a2f955
Shubhamkmaurya <93081549+Shubhamkmaurya@users.noreply.github.com>
2022-04-30 21:22:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,44 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+
+
 const App = () => {
 
+  const [shape, setShape] = useState("circle");
+  const [count, setCount] = useState(0);
+  
+   //let count=0;
+  const clickHandle = () => {
+    const body = document.getElementById("shape-")
+    const div = document.createElement("div")  
+
+    div.innerText = count
+      div.className = shape
+      body.appendChild(div)
+      setCount(count+1)   
+  }
+  
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select value={shape} onChange={(e) => setShape(e.target.value)}>
+          <option value="circle">
+            Circle
+          </option>
+          <option value="square">
+            Square
+          </option>
+        </select>
       </div>
+
       <div id="shapes-holder">
+        <button onClick={clickHandle}>Add Shape</button>
+      </div>
+      <div id="shape-">
+
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
